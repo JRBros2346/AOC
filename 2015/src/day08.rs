033eac2265c6b4ee2bb6c25868890af936da900f
Samuel Jabez P. <85472438+JRBros2346@users.noreply.github.com>
2025-11-10 20:57:25 +0530

    2015 - 8/25

--- 2015/src/day08.rs
+++ 2015/src/day08.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("TODO: aoc2015 day08");
+    fn decode(s: &str) -> usize {
+        let mut cnt = 0;
+        let mut char = s.chars();
+        while let Some(c) = char.next() {
+            if c == '"' {
+                continue;
+            }
+            if c == '\\' {
+                let next = char.next().unwrap();
+                if next == 'x' {
+                    char.next();
+                    char.next();
+                }
+            }
+            cnt += 1;
+        }
+        cnt
+    }
+    fn encode(s: &str) -> usize {
+        let mut cnt = 0;
+        for c in s.chars() {
+            if c == '"' || c == '\\' {
+                cnt += 1;
+            }
+            cnt += 1;
+        }
+        cnt + 2
+    }
+    let mut dcount = 0;
+    let mut ecount = 0;
+    for s in include_str!("input/day08.txt").trim().lines() {
+        dcount += s.len() - decode(s);
+        ecount += encode(s) - s.len();
+    }
+    println!("{dcount}");
+    println!("{ecount}");
 }
--- 2015/src/day28.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("TODO: aoc2015 day28");
-}
