91556e286fe0153a87281a211a04d56a193094c1
Samuel Jabez P. <85472438+JRBros2346@users.noreply.github.com>
2025-11-12 20:16:34 +0530

    2015 - 10/25

--- 2015/src/day10.rs
+++ 2015/src/day10.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("TODO: aoc2015 day10");
+    let mut input = include_str!("input/day10.txt").trim().to_owned();
+    for _ in 0..40 {
+        input = look_say(&input);
+    }
+    println!("{}", input.len());
+    for _ in 0..10 {
+        input = look_say(&input);
+    }
+    println!("{}", input.len());
+}
+
+fn look_say(input: &str) -> String {
+    let mut result = String::new();
+    let mut chars = input.chars().peekable();
+    while let Some(ch) = chars.next() {
+        let mut count = 1;
+        while let Some(&mext) = chars.peek() {
+            if mext == ch {
+                chars.next();
+                count += 1;
+            } else {
+                break;
+            }
+        }
+        result.push_str(&count.to_string());
+        result.push(ch);
+    }
+    result
 }
